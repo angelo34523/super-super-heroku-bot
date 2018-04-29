3c013f2e83bd35701832cda7472f1981c4eb5bd9
angelo34523 <37232416+angelo34523@users.noreply.github.com>
2018-04-29 11:44:33 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,6 +16,25 @@ client.on('message', message => {
 	message.reply('Ola, eu sou o angelbot desenvolvido por angelo gabriel kkk, voce deve tar se perguntando oque eu fasso certo? e eu te respondo nada!!!');
   	}
 });
-      
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!entrar'):
+      try:
+        canal = message.author.voice.voice_channel
+        await client.join_voice_channel(canal)
+      except discord.errors.InvalidArgument:
+             await client.send_message(message.channel, "Você precisa esta conectado a um canal de voz!")
+
+    if message.content.startswith('!sair'):
+      try:
+        canaldevoz = client.voice_client_in(message.server)
+        await canaldevoz.disconnect()
+      except AttributeError:
+          await client.send_message(message.channel,"O bot não esta conectado em nenhum canal de voz!")
+
+});
+
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

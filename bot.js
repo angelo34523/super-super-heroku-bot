9fb071d2f8dc40920d92521db53b8ad756be4e4f
angelo34523 <37232416+angelo34523@users.noreply.github.com>
2018-04-29 11:34:41 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,10 +10,12 @@ client.on('message', message => {
     	message.reply('pong');
   	}
 });
+
 client.on('message', message => {
     if (message.content === '!ping') {
 	message.reply('pong');
   	}
+});
       
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

8cc188b6127734f0ad9b9b750022fa0919226574
angelo34523 <37232416+angelo34523@users.noreply.github.com>
2018-04-29 11:33:52 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,20 +13,7 @@ client.on('message', message => {
 client.on('message', message => {
     if (message.content === '!ping') {
 	message.reply('pong');
-      try:
-        canal = message.author.voice.voice_channel
-        await client.join_voice_channel(canal)
-      except discord.errors.InvalidArgument:
-             await client.send_message(message.channel, "Você precisa esta conectado a um canal de voz!")
-
-    if message.content.startswith('!sair'):
-      try:
-        canaldevoz = client.voice_client_in(message.server)
-        await canaldevoz.disconnect()
-      except AttributeError:
-          await client.send_message(message.channel,"O bot não esta conectado em nenhum canal de voz!")
-		  
-});
-
+  	}
+      
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

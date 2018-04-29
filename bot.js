71a3044bc01481c742d2b9e2cd35b4bdfcfac396
angelo34523 <37232416+angelo34523@users.noreply.github.com>
2018-04-29 14:31:22 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ async ()
     print("Bot online - Olá Mundo!")
 
 client.event
-async (message):
+async (message)
     if message.content.startswith('!entrar'):
       try:
         canal = message.author.voice.voice_channel

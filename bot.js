812e08d25f7e059403f7c4a243831a8a1e131803
angelo34523 <37232416+angelo34523@users.noreply.github.com>
2018-04-29 12:52:21 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,19 @@ client.on('message', message => {
   	}
 });
 
+});
+
+mybot.on("message", function(message) {
+    if (message.content === "!live") {
+        getJSON("https://api.twitch.tv/kraken/streams/lalicel", function(err, res) {
+            if (res.stream == null) {
+                mybot.reply(message, "she is currently not live");
+            } else {
+                mybot.reply(message, "she is currently live");
+				mybot.sendMessage(message, "https://www.twitch.tv/lalicel");
+            }
+});
+        
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

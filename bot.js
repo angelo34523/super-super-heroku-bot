13a2a383950f73a68669d8963cbd04d9af8b340d
angelo34523 <37232416+angelo34523@users.noreply.github.com>
2018-04-29 14:24:02 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,31 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('VO LA SABER, NAO SOU VIDENTE PRA SABER PORRA');
-  	}
-});      
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(client.user.name)
+    print("Bot online - Olá Mundo!")
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!entrar'):
+      try:
+        canal = message.author.voice.voice_channel
+        await client.join_voice_channel(canal)
+      except discord.errors.InvalidArgument:
+             await client.send_message(message.channel, "Você precisa esta conectado a um canal de voz!")
+
+    if message.content.startswith('!sair'):
+      try:
+        canaldevoz = client.voice_client_in(message.server)
+        await canaldevoz.disconnect()
+      except AttributeError:
+          await client.send_message(message.channel,"O bot não esta conectado em nenhum canal de voz!")
+
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

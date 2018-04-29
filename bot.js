cb6defe95e7957d5b9a2404da2aca6082fa1ecad
angelo34523 <37232416+angelo34523@users.noreply.github.com>
2018-04-29 14:04:29 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,12 @@ var Queue = require('./components/queue.js');
 var TrackHelper = require('./components/trackhelper.js');
 var WordService = require('./components/wordservice.js');
 var WeatherService = require('./components/weatherservice.js');
+var express = require('express')
+var app = express()
+var ver ="0.86"
+var mybot = new Discord.Client();
+var getJSON = require('get-JSON');
+
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -17,11 +23,41 @@ client.on('message', message => {
   	}
 });
 
-},
-'!roll'
-  execute: roll,
-  description: 'roll from 1-100'
-      
+var commands = {
+  '!video': {
+    execute: getVideo,
+    description: 'get a youtube video by search word'
+  },
+  '!weather': {
+    execute: getWeather,
+    description: 'get current weather for the given city, defaults to Stockholm'
+  },
+  '!roll': {
+    execute: roll,
+    description: 'roll from 1-100'
+  },
+  '!help': {
+    execute: showHelp
+  },
+  '!words': {
+    execute: countWordsByUser,
+    description: 'get the most popular words for user of the given username, defaults to your username'
+  },
+  '!queue': {
+    execute: doQueue,
+    description: 'queue your song'
+  },
+  '!voteskip': {
+    execute: voteSkip,
+    description: 'vote to skip the current song'
+  },
+  '!song': {
+    execute: showSong,
+    description: 'get the current song'
+  }
+};
+
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

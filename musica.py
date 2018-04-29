94fbb696d1eb2f6d1d0136117521e11d024f6eeb
angelo34523 <37232416+angelo34523@users.noreply.github.com>
2018-04-29 16:10:57 -0300

    Update musica.py

--- musica.py
+++ musica.py
@@ -1,18 +1,1 @@
-import discord
-import asyncio
-import random
 
-client = discord.Client()
-
-@client.event
-async def on_message (message):
-    if message.content.lower().startswith('!test'):
-        await client.send_message(message.channel, "Ola mundo, estou vivo.")
-
-
-    if message.content.lower().startswith('!moeda'):
-      choice = random.randint(1,2)
-      if choice == 1:
-       await client.add_reaction(message, 'ðŸ˜€')
-      if choice == 2:
-       await client.add_reaction(message, 'ðŸ¤´')

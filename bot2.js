bd4e13a8f2f2bf79db52176abde943255f66c91e
angelo34523 <37232416+angelo34523@users.noreply.github.com>
2018-04-29 12:30:05 -0300

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -1,1 +1,18 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
+client.on('ready', () => {
+    console.log('I am ready!');
+
+client.on('message', message => {
+    if (message.content === 'bunda') {
+    	message.reply('pong3');
+  	}
+
+client.on('message', message => {
+    if (message.content === '!test') {
+	message.reply('Ola, eu sou o angelbot desenvolvido por angelo gabriel kkk, voce deve tar se perguntando oque eu fasso certo? e eu te respondo nada!!!');
+  	}
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);

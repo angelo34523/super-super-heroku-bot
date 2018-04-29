a766babdd7f1e81dd2e55ea1ec87a4fb8f3e6040
angelo34523 <37232416+angelo34523@users.noreply.github.com>
2018-04-29 16:48:06 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,2 +1,22 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import os
 
+
+
+bot = commands.Bot(command_prefix="a.")
+
+
+
+@bot.event
+async def on_ready():
+  print(bot.user.name)
+
+  
+  
+@bot.command(pass_context=True)
+async def hi(ctx):
+  await bot.say("Hello there"+" "+ctx.message.author.name)
 

7cb21710d4970d96d546fac11af72c06c5f6bebc
angelo34523 <37232416+angelo34523@users.noreply.github.com>
2018-04-29 17:21:18 -0300

    Update musica.py

--- musica.py
+++ musica.py
@@ -1,80 +1,58 @@
 import time
-import subprocess
+import discord
+import psutil
+import os
 
-from utils import repo, default
 from discord.ext import commands
+from utils import repo, default
 
 
-class Admin:
+class Information:
     def __init__(self, bot):
         self.bot = bot
         self.config = default.get("config.json")
-        self._last_result = None
+        self.process = psutil.Process(os.getpid())
 
     @commands.command()
-    async def amiadmin(self, ctx):
-        """ Are you admin? """
-        if ctx.author.id in repo.owners:
-            return await ctx.send(f"Yes **{ctx.author.name}** you are admin! ✅")
+    async def ping(self, ctx):
+        """ Pong! """
+        before = time.monotonic()
+        message = await ctx.send("Pong")
+        ping = (time.monotonic() - before) * 1000
+        await message.edit(content=f"Pong   |   {int(ping)}ms")
 
-        # Please do not remove this part.
-        # I would love to be credited as the original creator of the source code.
-        if ctx.author.id == 86477779717066752:
-            return await ctx.send(f"Well kinda **{ctx.author.name}**.. you still own the source code")
-
-        await ctx.send(f"no, heck off {ctx.author.name}")
-
-    @commands.command()
-    @commands.check(repo.is_owner)
-    async def reload(self, ctx, name: str):
-        """ Reloads an extension. """
-        try:
-            self.bot.unload_extension(f"cogs.{name}")
-            self.bot.load_extension(f"cogs.{name}")
-        except Exception as e:
-            await ctx.send(f"```\n{e}```")
-            return
-        await ctx.send(f"Reloaded extension **{name}.py**")
+    @commands.command(aliases=['joinme', 'join', 'botinvite'])
+    async def invite(self, ctx):
+        """ Invite me to your server """
+        await ctx.send(f"**{ctx.author.name}**, use this URL to invite me\n<{discord.utils.oauth_url(self.bot.user.id)}>")
 
     @commands.command()
-    @commands.check(repo.is_owner)
-    async def reboot(self, ctx):
-        """ Reboot the bot """
-        await ctx.send('Rebooting now...')
-        time.sleep(1)
-        await self.bot.logout()
+    async def source(self, ctx):
+        """ Invite me to your server """
+        await ctx.send(f"**{ctx.bot.user}** is powered by this source code:\nhttps://github.com/xelA/discord_bot.py")
 
-    @commands.command()
-    @commands.check(repo.is_owner)
-    async def load(self, ctx, name: str):
-        """ Reloads an extension. """
-        try:
-            self.bot.load_extension(f"cogs.{name}")
-        except Exception as e:
-            await ctx.send(f"```diff\n- {e}```")
-            return
-        await ctx.send(f"Loaded extension **{name}.py**")
+    @commands.command(aliases=['supportserver', 'feedbackserver'])
+    async def botserver(self, ctx):
+        """ Get an invite to our support server! """
+        if isinstance(ctx.channel, discord.DMChannel) or ctx.guild.id != 86484642730885120:
+            return await ctx.send(f"**Here you go {ctx.author.name} 🍻\n<{repo.invite}>**")
 
-    @commands.command()
-    @commands.check(repo.is_owner)
-    async def unload(self, ctx, name: str):
-        """ Reloads an extension. """
-        try:
-            self.bot.unload_extension(f"cogs.{name}")
-        except Exception as e:
-            await ctx.send(f"```diff\n- {e}```")
-            return
-        await ctx.send(f"Unloaded extension **{name}.py**")
+        await ctx.send(f"**{ctx.author.name}** this is my home you know :3")
 
-    @commands.command(aliases=['exec'])
-    @commands.check(repo.is_owner)
-    async def execute(self, ctx, *, text: str):
-        """ Do a shell command. """
-        text_parsed = list(filter(None, text.split(" ")))
-        output = subprocess.check_output(text_parsed).decode()
-        await ctx.send(f"```fix\n{output}\n```")
+    @commands.command(aliases=['info', 'stats', 'status'])
+    async def about(self, ctx):
+        """ About the bot """
+        ramUsage = self.process.memory_full_info().rss / 1024**2
+
+        embed = discord.Embed(colour=ctx.me.top_role.colour)
+        embed.set_thumbnail(url=ctx.bot.user.avatar_url)
+        embed.add_field(name="Developer", value="AlexFlipnote#0001", inline=True)
+        embed.add_field(name="Library", value="discord.py", inline=True)
+        embed.add_field(name="Servers", value=len(ctx.bot.guilds), inline=True)
+        embed.add_field(name="RAM", value=f"{ramUsage:.2f} MB", inline=True)
+
+        await ctx.send(content=f"ℹ About **{ctx.bot.user}** | **{repo.version}**", embed=embed)
 
 
 def setup(bot):
-bot.add_cog(Admin(bot))
-
+bot.add_cog(Information(bot))

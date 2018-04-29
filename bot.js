6667ed6e6d55e68229f7808042e28bbe400f201e
angelo34523 <37232416+angelo34523@users.noreply.github.com>
2018-04-29 14:09:49 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var Bot = new Discord.Client();
-var Helper = require('./components/helper.js');
-var Queue = require('./components/queue.js');
-var TrackHelper = require('./components/trackhelper.js');
-var WordService = require('./components/wordservice.js');
-var WeatherService = require('./components/weatherservice.js');
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === 'junior') {
-    	message.reply('VIADO!!!');
+    if (message.content === 'ping') {
+    	message.reply('VO LA SABER, NAO SOU VIDENTE PRA SABER PORRA');
   	}
 });      
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
+

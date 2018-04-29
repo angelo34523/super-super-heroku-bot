f7714820c40b9c5ec006ad843cbae4e5d365afd4
angelo34523 <37232416+angelo34523@users.noreply.github.com>
2018-04-29 11:55:50 -0300

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -14,11 +14,11 @@ async def on_ready ():
 
 @client.event
 async def on_message (message):
-    if message.content.lower().startswith('!test'):
+    if message.content.lowet().startswith('!test'):
         await client.send_message(message.channel, "Ola mundo, estou vivo.")
 
 
-    if message.content.lower().startswith('!moeda'):
+    if message.content.lowet().startswith('!moeda'):
       choice = random.randint(1,2)
       if choice == 1:
        await client.add_reaction(message, 'ðŸ˜€')

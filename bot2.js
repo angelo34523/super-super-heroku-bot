1446f8e63fa9aa79d6a94c5ab5c392038db8f695
angelo34523 <37232416+angelo34523@users.noreply.github.com>
2018-04-29 11:55:10 -0300

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -1,22 +1,32 @@
+import discord
+import asyncio
+import random
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
+client = discord.Client()
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
+@client.event
+async def on_ready ():
+    print ('BOT ONLINE - Ola Mundo!')
+    print (client.user.name)
+    print (client.user.id)
+    print ('----------BM--------')
 
-client.on('message', message => {
-    if (message.content === 'vidaloka') {
-    	message.reply('pongi');
-  	}
-});
 
-client.on('message', message => {
-    if (message.content === '!angelbot4') {
-	message.reply('Ola, eu sou o angelbot desenvolvido por angelo gabriel kkk, voce deve tar se perguntando oque eu fasso certo? e eu te respondo nada!!!');
-  	}
-});
+@client.event
+async def on_message (message):
+    if message.content.lower().startswith('!test'):
+        await client.send_message(message.channel, "Ola mundo, estou vivo.")
+
+
+    if message.content.lower().startswith('!moeda'):
+      choice = random.randint(1,2)
+      if choice == 1:
+       await client.add_reaction(message, 'ðŸ˜€')
+      if choice == 2:
+       await client.add_reaction(message, 'ðŸ¤´')
+
+
+
       
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

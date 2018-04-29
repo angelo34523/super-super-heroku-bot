5f6d52841b6131e1c11879d19b8d775d3de62fdd
angelo34523 <37232416+angelo34523@users.noreply.github.com>
2018-04-29 12:00:54 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,28 +16,6 @@ client.on('message', message => {
 	message.reply('Ola, eu sou o angelbot desenvolvido por angelo gabriel kkk, voce deve tar se perguntando oque eu fasso certo? e eu te respondo nada!!!');
   	}
 	
-	
-@client.on
-client.on on_ready ():
-    print ('BOT ONLINE - Ola Mundo!')
-    print (client.user.name)
-    print (client.user.id)
-    print ('----------BM--------')
 
-
-@client.on
-client.on def on_message (message):
-    if message.content.lower().startswith('!test'):
-        await client.send_message(message.channel, "Ola mundo, estou vivo.")
-
-
-    if message.content.lower().startswith('!moeda'):
-      choice = random.randint(1,2)
-      if choice == 1:
-       await client.add_reaction(message, 'ðŸ˜€')
-      if choice == 2:
-       await client.add_reaction(message, 'ðŸ¤´')
-
-	
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

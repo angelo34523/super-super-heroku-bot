415c1fda65d6ef460dc15f7f2c4161c5692ac0d7
angelo34523 <37232416+angelo34523@users.noreply.github.com>
2018-04-29 17:19:38 -0300

    Update musica.py

--- musica.py
+++ musica.py
@@ -1,16 +1,80 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+import time
+import subprocess
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
+from utils import repo, default
+from discord.ext import commands
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('VO LA SABER, NAO SOU VIDENTE PRA SABER PORRA');
-  	}
-});      
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+class Admin:
+    def __init__(self, bot):
+        self.bot = bot
+        self.config = default.get("config.json")
+        self._last_result = None
+
+    @commands.command()
+    async def amiadmin(self, ctx):
+        """ Are you admin? """
+        if ctx.author.id in repo.owners:
+            return await ctx.send(f"Yes **{ctx.author.name}** you are admin! ✅")
+
+        # Please do not remove this part.
+        # I would love to be credited as the original creator of the source code.
+        if ctx.author.id == 86477779717066752:
+            return await ctx.send(f"Well kinda **{ctx.author.name}**.. you still own the source code")
+
+        await ctx.send(f"no, heck off {ctx.author.name}")
+
+    @commands.command()
+    @commands.check(repo.is_owner)
+    async def reload(self, ctx, name: str):
+        """ Reloads an extension. """
+        try:
+            self.bot.unload_extension(f"cogs.{name}")
+            self.bot.load_extension(f"cogs.{name}")
+        except Exception as e:
+            await ctx.send(f"```\n{e}```")
+            return
+        await ctx.send(f"Reloaded extension **{name}.py**")
+
+    @commands.command()
+    @commands.check(repo.is_owner)
+    async def reboot(self, ctx):
+        """ Reboot the bot """
+        await ctx.send('Rebooting now...')
+        time.sleep(1)
+        await self.bot.logout()
+
+    @commands.command()
+    @commands.check(repo.is_owner)
+    async def load(self, ctx, name: str):
+        """ Reloads an extension. """
+        try:
+            self.bot.load_extension(f"cogs.{name}")
+        except Exception as e:
+            await ctx.send(f"```diff\n- {e}```")
+            return
+        await ctx.send(f"Loaded extension **{name}.py**")
+
+    @commands.command()
+    @commands.check(repo.is_owner)
+    async def unload(self, ctx, name: str):
+        """ Reloads an extension. """
+        try:
+            self.bot.unload_extension(f"cogs.{name}")
+        except Exception as e:
+            await ctx.send(f"```diff\n- {e}```")
+            return
+        await ctx.send(f"Unloaded extension **{name}.py**")
+
+    @commands.command(aliases=['exec'])
+    @commands.check(repo.is_owner)
+    async def execute(self, ctx, *, text: str):
+        """ Do a shell command. """
+        text_parsed = list(filter(None, text.split(" ")))
+        output = subprocess.check_output(text_parsed).decode()
+        await ctx.send(f"```fix\n{output}\n```")
+
+
+def setup(bot):
+bot.add_cog(Admin(bot))
 

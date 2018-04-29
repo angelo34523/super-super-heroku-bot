c989dbc73650cf353da1469f9955729da1481106
angelo34523 <37232416+angelo34523@users.noreply.github.com>
2018-04-29 12:55:15 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,21 +9,7 @@ client.on('message', message => {
     if (message.content === 'vidaloka') {
     	message.reply('VIADO!!!');
   	}
-});
-
-});
-
-mybot.on("message", function(message) {
-    if (message.content === "_live") {
-        getJSON("https://api.twitch.tv/kraken/streams/lalicel", function(err, res) {
-            if (res.stream == null) {
-                mybot.reply(message, "she is currently not live");
-            } else {
-                mybot.reply(message, "she is currently live");
-				mybot.sendMessage(message, "https://www.twitch.tv/lalicel");
-            }
-});
-        
+});      
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
